b9717d261ae009a6aeedf808248e802d55c06ad2
Jesus Bazan <jesus.bazan@gmail.com>
2023-08-14 15:19:41 -0500

    Task 3: Display data visually for traders

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -10,7 +10,8 @@ export interface ServerRespond {
 }
 
 class DataStreamer {
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  //static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL: string = 'http://104.243.38.81:8080/query?id=1';
 
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
